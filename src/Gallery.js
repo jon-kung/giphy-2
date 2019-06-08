851176fc7aef7196437464077fc620edb55c3299
Jonathan <jkung92@gmail.com>
2019-06-07 20:12:35 -0700

    adding gallery

---
+++ src/Gallery.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+ 
+  class Gallery extends Component {
+    render() {
+      return (
+        <div className="container">
+          <ul>
+            {/* map over gifs that are passed here */}
+            <li></li>
+          </ul>
+        </div>
+      );
+    }
+  }
+ 
+export default Gallery;
