82895892dc35f7a96afb00e0051be23d9cd8154f
Jonathan <jkung92@gmail.com>
2019-06-07 20:15:56 -0700

    adding placeholders

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
  
   class Gallery extends Component {
+    constructor(props) {
+      super(props);
+      this.state = { trendingGifs: [] };
+      this.renderTrendingGifs = this.renderTrendingGifs.bind(this);
+    }
+  
+    renderTrendingGifs() {
+      return (
+        <ul>
+          {this.state.trendingGifs.map(gif => (
+            <li key={gif.id}>
+              {/* TODO: RENDER GIF HERE , LOOK AT JSON */}
+            </li>
+          ))}
+        </ul>
+      );
+    }
+
     render() {
       return (
         <div className="container">
