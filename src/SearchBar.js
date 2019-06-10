3c37050c48aa3f13ca26aa98665f357ef425821d
Jonathan <jkung92@gmail.com>
2019-06-10 14:54:52 -0700

    className

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
   render() {
     return (
-      <form action="/search" class="form-inline justify-content-center">
-        <div class="form-group m-1">
-          <input class="form-control m-1" type="text" name="search" />
-          <button class="btn btn-secondary rounded m-1" name="searchButton">
+      <form action="/search" className="form-inline justify-content-center">
+        <div className="form-group m-1">
+          <input className="form-control m-1" type="text" name="search" />
+          <button className="btn btn-secondary rounded m-1" name="searchButton">
             Search Giphy!
           </button>
         </div>
