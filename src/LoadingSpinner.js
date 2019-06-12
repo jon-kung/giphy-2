ea34e17b89e26ca0fd535c51b323e286410c365b
Jonathan <jkung92@gmail.com>
2019-06-12 15:59:02 -0700

    loading spinner

---
+++ src/LoadingSpinner.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class LoadingSpinner extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <i className="fas fa-spinner fa-3x" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LoadingSpinner;
