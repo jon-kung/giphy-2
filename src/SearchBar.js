aa9ebfee9d0f547d29a68f91aba589ea9bccd78d
Jonathan <jkung92@gmail.com>
2019-06-11 17:05:38 -0700

    adding search functionality

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { searchQuery: '' };
+  }
+
+  handleChange = evt => {
+    this.setState({
+      searchQuery: evt.target.value
+    });
+  };
+
+  handleSubmit = evt => {
+    evt.preventDefault();
+    this.props.getSearchResults(this.state.searchQuery);
+    this.setState({ searchQuery: '' });
+  };
+
   render() {
     return (
-      <form action="/search" className="form-inline justify-content-center">
+      <form
+        onSubmit={this.handleSubmit}
+        className="form-inline justify-content-center"
+      >
         <div className="form-group m-1">
-          <input className="form-control m-1" type="text" name="search" />
-          <button className="btn btn-secondary rounded m-1" name="searchButton">
+          <input
+            className="form-control m-1"
+            type="text"
+            name="search"
+            placeholder="Search for GIFS here!"
+            onChange={this.handleChange}
+          />
+          <button
+            type="submit"
+            className="btn btn-secondary rounded m-1"
+            name="searchButton"
+          >
             Search Giphy!
           </button>
         </div>
