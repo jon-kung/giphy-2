5c1c29f90a7bc4fe3cf625e065a3f76bc3fa374c
Jonathan <jkung92@gmail.com>
2019-06-27 14:57:32 -0700

    added onClick addFavorites

--- src/GifDetails.js
+++ src/GifDetails.js
@@ -1,27 +1,24 @@
 import React, { Component } from 'react';
-import LoadingSpinner from './LoadingSpinner';
+// import LoadingSpinner from './LoadingSpinner';
 
 class GifDetails extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { isLoading: true };
-  }
-
-  componentDidMount() {
-    this.setState({ isLoading: false });
-  }
-
-  // handleAddFavorite = gifObject => {
-  //   this.props.addFavorite(gifObject)
+  // Individual place holder (spinner) not working
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { isLoading: true };
   // }
 
+  // componentDidMount() {
+  //   this.setState({ isLoading: false });
+  // }
+
+  handleAddFavorite = gifObject => {
+    this.props.addFavorite(gifObject);
+  };
 
   renderGif = () => {
     let { gif } = this.props;
-
-    return this.state.isLoading ? (
-      <LoadingSpinner />
-    ) : (
+    return (
       <div>
         <img
           className="image"
@@ -31,11 +28,11 @@ class GifDetails extends Component {
         />
         <div className="overlay">
           <div className="details">
-            <p> Title: {gif.title} </p>
+            <p className="gif-title"> {gif.title.toUpperCase()} </p>
             <p> Username: {gif.username ? gif.username : 'Anonymous'} </p>
             <p> Rating: {gif.rating.toUpperCase()} </p>
             <p> Source: {gif.source_tld ? gif.source_tld : 'Unknown'} </p>
-            {/* <i class="fas fa-heart" onClick={this.handleAddFavorite(gif)}></i> */}
+            <i className="far fa-heart" onClick={() => this.handleAddFavorite(gif)} ></i>
           </div>
         </div>
       </div>
@@ -43,11 +40,7 @@ class GifDetails extends Component {
   };
 
   render() {
-    return (
-      <div className="wrapper">
-        {this.renderGif()}
-      </div>
-    );
+    return <div className="wrapper">{this.renderGif()}</div>;
   }
 }
 
